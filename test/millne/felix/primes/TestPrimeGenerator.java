3fc732dda8f310818a68c4bed120da87a660c31b
Felix Millne <felix.millne@btinternet.com>
2017-12-07 00:58:39 +0000

    Add test for large (100) number of prime generation - using this to guarantee general algorithm correctness

--- test/millne/felix/primes/TestPrimeGenerator.java
+++ test/millne/felix/primes/TestPrimeGenerator.java
@@ -2,6 +2,8 @@ package millne.felix.primes;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -59,5 +61,28 @@ public class TestPrimeGenerator {
 
         Integer thirdPrime = 5;
         assertEquals(thirdPrime, primesResult.get(2));
+    }
+
+    @Test
+    public void testHundredPrimes() {
+        int testNumPrimes = 100;
+        ErastothenesPrimeGenerator testPrimeGenerator = new ErastothenesPrimeGenerator();
+        List<Integer> primesResult = testPrimeGenerator.getPrimes(testNumPrimes);
+
+        assertEquals(testNumPrimes, primesResult.size());
+        List<Integer> controlPrimes = Arrays.asList(
+                2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+                31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
+                73, 79, 83, 89, 97, 101, 103, 107, 109,
+                113, 127, 131, 137, 139, 149, 151, 157, 
+                163, 167, 173, 179, 181, 191, 193, 197,
+                199, 211, 223, 227, 229, 233, 239, 241,
+                251, 257, 263, 269, 271, 277, 281, 283,
+                293, 307, 311, 313, 317, 331, 337, 347,
+                349, 353, 359, 367, 373, 379, 383, 389,
+                397, 401, 409, 419, 421, 431, 433, 439,
+                443, 449, 457, 461, 463, 467, 479, 487,
+                491, 499, 503, 509, 521, 523, 541);
+        assertEquals(controlPrimes, primesResult);
     }
 }

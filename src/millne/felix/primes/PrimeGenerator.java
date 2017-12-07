5b4286f3664a354dcb24e6b413b0baa156b80d6b
Felix Millne <felix.millne@btinternet.com>
2017-12-07 00:13:36 +0000

    Add basic zero test and dummy method (please assume a breaking test/fix test implementation has been used here, I leave this out for the sake of sanity)

---
+++ src/millne/felix/primes/PrimeGenerator.java
@@ -0,0 +1,12 @@
+package millne.felix.primes;
+
+import java.util.ArrayList;
+
+/**
+ * Created by FelixMarcus on 07/12/2017.
+ */
+public class PrimeGenerator {
+    public ArrayList<Integer> getPrimes(int testNumPrimes) {
+        return new ArrayList<Integer>();
+    }
+}
--- test/TestPrimeGenerator.java
+++ test/TestPrimeGenerator.java
@@ -1,7 +1,19 @@
+import millne.felix.primes.PrimeGenerator;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 /**
  * Created by FelixMarcus on 07/12/2017.
  */
 public class TestPrimeGenerator {
 
-    
+    @Test
+    public void testZeroPrimes(){
+        int testNumPrimes = 0;
+        PrimeGenerator testPrimeGenerator = new PrimeGenerator();
+        ArrayList<Integer> primesResult = testPrimeGenerator.getPrimes(testNumPrimes);
+        Assert.assertEquals(0, primesResult.size());
+    }
 }
